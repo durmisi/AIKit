0b410d2fbcfbc7e33736b2a73a1de9c26c68cf56
Admir Durmishi <durmisi@gmail.com>
2026-01-29 12:50:34 +0100

    Refactor: Add XML documentation for the Constants class in GitHub client

--- Clients/AIKit.Clients.GitHub/Constants.cs
+++ Clients/AIKit.Clients.GitHub/Constants.cs
@@ -1,5 +1,8 @@
 ﻿namespace AIKit.Clients.GitHub;
 
+/// <summary>
+/// Contains constant values used by the GitHub client.
+/// </summary>
 internal class Constants
 {
     public const string GitHubModelsEndpoint = "https://models.github.ai/inference";
